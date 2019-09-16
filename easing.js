e76bac65c486c79a13a5e53d3f189d807cec3f75
Lutz Reiter <lutz.reiter@vw-fc.com>
2019-09-16 11:59:30 +0200

    added new easing functions

--- easing.js
+++ easing.js
@@ -1,32 +1,36 @@
 const _ = require('lodash');
 
 EasingFunctions = {
-    // no easing, no acceleration
     linear: function (t) { return t },
-    // accelerating from zero velocity
+    
     easeInQuad: function (t) { return t*t },
-    // decelerating to zero velocity
     easeOutQuad: function (t) { return t*(2-t) },
-    // acceleration until halfway, then deceleration
     easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
-    // accelerating from zero velocity 
+
     easeInCubic: function (t) { return t*t*t },
-    // decelerating to zero velocity 
     easeOutCubic: function (t) { return (--t)*t*t+1 },
-    // acceleration until halfway, then deceleration 
     easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
-    // accelerating from zero velocity 
+
     easeInQuart: function (t) { return t*t*t*t },
-    // decelerating to zero velocity 
     easeOutQuart: function (t) { return 1-(--t)*t*t*t },
-    // acceleration until halfway, then deceleration
     easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
-    // accelerating from zero velocity
+
     easeInQuint: function (t) { return t*t*t*t*t },
-    // decelerating to zero velocity
     easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
-    // acceleration until halfway, then deceleration 
-    easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+    easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t },
+
+    easeInSine: function (t) { return Math.cos(t * (Math.PI/2)); },
+	easeOutSine: function (t) { return Math.sin(t * (Math.PI/2)); },
+    easeInOutSine: function (t) { return Math.cos(Math.PI*t) - 1; },
+    
+    easeInExpo: function (t) { return (t==0) ? 0 : Math.pow(2, 10 * (t - 1)); },
+	easeOutExpo: function (t) { return (t==1.0) ? 1.0 : (-Math.pow(2, -10 * t) + 1); },
+	easeInOutExpo: function (t) {
+		if (t==0) return 0;
+		if (t==1.0) return 1.0;
+        if ((t/2) < 1) return 1/2 * Math.pow(2, 10 * (t - 1));
+        else return 1/2 * (-Math.pow(2, -10 * --t) + 2);
+	},
 }
 
 function stopInterval(interval) {
